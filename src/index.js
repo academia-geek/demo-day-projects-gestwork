a206c9c7bc6fc0f8253d7df34f1201f4ca5e87a8
damarishernandez <cortezhernandezdamaris@gmail.com>
2022-03-19 21:21:07 -0500

    Structure initial folders and files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouters from '../Routers/AppRouters';
+import { GestWork } from './gestWork';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRouters />
+     <GestWork/>
   </React.StrictMode>,
   document.getElementById('root')
 );
