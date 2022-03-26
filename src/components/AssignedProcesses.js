85027df0deab0e046823513eeeaae6a80bffe699
anyelithj <anyelithj@misena.edu.co>
2022-03-25 20:20:08 -0500

    se arreglo las rutas y diseño de menu y navBar

--- src/components/AssignedProcesses.js
+++ src/components/AssignedProcesses.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import {Trash } from 'grommet-icons';
 
 export const AssignedProcesses = () => {
   return (
     <>
-    <div>
-      <h2>Procesos Asignados</h2>
+    <div className="container">
+      <div>
+        <h2 className='title__section'>Procesos Asignados</h2>
+      </div>
+      <p>Mira aquí los procesos que te han sido asignados</p>
+      <section>
+        <ul>
+          <li className='item__assigned'>Proceso N° 45 <span className='item__assigned--link'><Trash color='plain' size='large' /> </span></li>
+          <li className='item__assigned'>Proceso N° 31 <span className='item__assigned--link'><Trash color='plain' size='large' /> </span></li>
+          <li className='item__assigned'>Proceso N° 45 <span className='item__assigned--link'><Trash color='plain' size='large' /> </span></li>
+          <li className='item__assigned'>Proceso N° 52 <span className='item__assigned--link'><Trash color='plain' size='large' /> </span></li>
+        </ul>
+      </section>
     </div>
-    <p>Mira aquí los procesos que te han sido asignados</p>
-    <section>
-       <ul>
-         <li>Proceso N° 45 <span>eliminar icon</span></li>
-         <li>Proceso N° 31 <span>eliminar icon</span></li>
-         <li>Proceso N° 45 <span>eliminar icon</span></li>
-         <li>Proceso N° 52 <span>eliminar icon</span></li>
-       </ul>
-    </section>
   </>
   )
 }
--- src/components/MenuNavigation.js
+++ src/components/MenuNavigation.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../styles/config.css'
 import '../styles/MenuNavigation.css'
+import {Logout, DocumentUpload, Copy} from 'grommet-icons';
+import { Link } from 'react-router-dom';
 
 
 export const MenuNavigation = () => {
   return (
-    <ul>
-      <li><a href="#home">Procesos Activos</a></li>
-      <li><a href="#news">Sube tu Procesos</a></li>
-      <li><a href="#contact">Salir</a></li>
+    <ul className='menu'>
+      <li><Link to='/activeProcesses' className='menu__item'><Copy size='large' />Procesos Activos</Link></li>
+      <li><Link to='/assignedProcesses' className='menu__item'><DocumentUpload size='large'/>Sube tu Procesos</Link></li>
+      <li><Link to='/' className='menu__item'><Logout size='large' />Salir</Link></li>
     </ul>
   )
 }
