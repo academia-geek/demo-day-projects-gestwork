d4d9d3aa092cfaea62c3c89e3b590557df8ae6ca
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-04-01 18:31:49 -0500

    Datos de usuario en el nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,73 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { Notification } from "grommet-icons";
+import React from "react";
+import { Container, Form, Navbar } from "react-bootstrap";
+import UserPerfil from "../hooks/UserPerfil";
 import "../styles/NavBar.css";
-import { Link } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase/firebaseConfig";
-import { useDispatch, useSelector } from "react-redux";
-import { Cargo } from "../redux/actions/actionLogin";
+
 
 
 export const NavBar = () => {
-  const [objtFav, setObjfav] = useState('Santiago Torres');
-  const [nombre, setObjnombre] = useState('');
 
-  let estados = useSelector((state) => {
-    return state;
-  });
-  console.log("ee")
-  console.log(estados)
-  let dispatch = useDispatch();
-
-  
-const listitem2 = async () => {
-console.log(estados)
-//console.log(estados.user.email)
-console.log("estados.user.email")
-  const querySnapshot = await getDocs(collection(db, "users"));
-  querySnapshot.forEach((doc) => {
-    if (doc.data().email === estados.login.user.email){
-     let cargo= doc.data().cargo
-      console.log("if cargo")
-      console.log(cargo)
-      //estados.user.cargo=cargo
-      setObjfav(cargo)
-      setObjnombre(estados.login.user.nombre
-        )
-    ///dispatch(Cargo(cargo));
-      
-    }   
-  });
-
-};
-
-const listitem = async () => {
-    setTimeout(()=>{listitem2()},3000)
-
-}
-listitem2()
-
-useEffect(() => {
-  //listitem()
-    console.log(estados,"ssssssssssssssssssssssssss")
-    //listitem()
-}, []);
+  const user = UserPerfil();
+  console.log(user)
 
   return (
     <Navbar bg="light" className='container-fluid d-flex justify-content-end t-0'>
       <Container fluid className='container-fluid d-flex justify-content-end t-0'>
-      <img style={{marginRight:"420px"}} src="https://res.cloudinary.com/df90q7vvj/image/upload/v1648513403/GestWork/Free_Sample_By_Wix_1_lvgyss.jpg" alt="logo"/>
+      <img style={{marginRight:"420px"}} src="https://res.cloudinary.com/df90q7vvj/image/upload/v1648513403/GestWork/Free_Sample_By_Wix_1_lvgyss.jpg" alt=""/>
 
           <Form className="sub_navBar">
             <div className="subtitle_navBar container-fluid d-flex justify-content-end">
-              <h4>{nombre}</h4>
-              <h4>{objtFav}</h4>
+              <h4> {user.nombre !== undefined
+                  ? user.nombre
+                  : (user.nombre = "")}</h4>
+              <h4> {user.cargo !== undefined
+                  ? user.cargo
+                  : (user.cargo = "")}</h4>
             </div>
             <div className="d-flex p-1">
               <img
                 className="phone__profile"
-                src="https://e7.pngegg.com/pngimages/272/184/png-clipart-businessperson-avatar-woman-business-avatar-microphone-black-hair.png"
+                src={user.foto}
              alt='' />
              
             </div>
