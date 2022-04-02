4da2bf49747aa05d1c0f45d1188109de17761019
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-04-01 19:48:11 -0500

    Manu hamburguer responsive

--- src/components/MenuNavigation.js
+++ src/components/MenuNavigation.js
@@ -5,6 +5,16 @@ import { Logout, DocumentUpload, Copy } from "grommet-icons";
 import { Link } from "react-router-dom";
 import { logoutInitiate } from "../redux/actions/actionLogin";
 import { useDispatch } from "react-redux";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Offcanvas,
+} from "react-bootstrap";
 
 export const MenuNavigation = () => {
   const dispatch = useDispatch();
@@ -41,6 +51,47 @@ export const MenuNavigation = () => {
           </Link>
         </li>
       </ul>
+
+      <Navbar className="menuResponsive" bg="light" expand={false}>
+        <Container fluid>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                GestWork
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="bg-dark">
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Link to="/activeProcesses" className="menu__item">
+                  <Copy size="large" />
+                  <label className="ms-2">Procesos Activos</label>
+                </Link>
+              </Nav>
+              <Link to="/" className="menu__item">
+                <DocumentUpload size="large" />
+                <label className="ms-2">Sube tus Procesos</label>
+              </Link>
+              <li>
+                <Link to="/assignedProcesses" className="menu__item">
+                  <Copy size="large" />
+                  <label className="ms-2">Procesos Asignados</label>
+                </Link>
+              </li>
+              <li onClick={handleLogout}>
+                <Link to="/" className="menu__item menu-logout">
+                  <Logout size="large" />
+                  <label className="ms-2">Salir</label>
+                </Link>
+              </li>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
   );
 };
