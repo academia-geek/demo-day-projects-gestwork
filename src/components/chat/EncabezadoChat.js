5f06e868ed402934fe69025fc3281606677aa15d
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-04-05 13:50:57 -0500

    Route send chat to menu

--- src/components/chat/EncabezadoChat.js
+++ src/components/chat/EncabezadoChat.js
@@ -1,12 +1,8 @@
 import React from "react";
-import {
-  Room,
-  PeopleAlt,
-  Send,
-} from "@material-ui/icons";
+import { Room, PeopleAlt, Send } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
-const EncabezadoChat = ({nombreCanal}) => {
+const EncabezadoChat = ({ nombreCanal }) => {
   return (
     <div className="chatHeader">
       <div className="chatHeader_left">
@@ -16,8 +12,12 @@ const EncabezadoChat = ({nombreCanal}) => {
         </h3>
       </div>
       <div className="chatHeader__right">
-        <Link to="/profile"><PeopleAlt /></Link>
-        <Send/>
+        <Link to="/profile">
+          <PeopleAlt />
+        </Link>
+        <Link to="/*">
+          <Send />
+        </Link>
       </div>
     </div>
   );
