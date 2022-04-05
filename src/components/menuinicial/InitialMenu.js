c29cdee2eb43391b7038ca03d1d499d2ac5e7039
Juliana Jimenez <nani.jimenez@hotmail.com>
2022-04-04 20:20:06 -0500

    navbarmenuinicial

--- src/components/menuinicial/InitialMenu.js
+++ src/components/menuinicial/InitialMenu.js
@@ -2,19 +2,49 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { NavBar } from '../NavBar'
 import { DocumentUpload, Copy, ChatOption, Attachment } from "grommet-icons";
 import './styleinitmenu.css';
 
+import { Container, Form, Navbar } from "react-bootstrap";
+import UserPerfil from "../../hooks/UserPerfil";
+
 const InitialMenu = () => {
+  const user = UserPerfil();
+  console.log(user)
+
   return (
 
 
     <div>
 
-    <div className="containerAdd navbarmenu">
+    <div  id='navbarmenu'>
 
-    <NavBar/>
+    <Navbar bg="light" className='container-fluid d-flex justify-content-end t-0' id='mm'>
+      <Container fluid className='container-fluid d-flex justify-content-end t-0'>
+     <Link to='/*'>  <img className="logoImg " style={{marginRight:"710px"}} src="https://res.cloudinary.com/df90q7vvj/image/upload/v1648513403/GestWork/Free_Sample_By_Wix_1_lvgyss.jpg" alt=""/>
+     </Link>
+          <Form className="sub_navBar">
+            <Link to="/profile" className="link-name">
+            <div className="subtitle_navBar container-fluid d-flex justify-content-end">
+              <h4> {user.nombre !== undefined
+                  ? user.nombre
+                  : (user.nombre = "")}</h4>
+            </div>
+            </Link>
+
+            <div className="d-flex p-1">
+              {user.foto !== undefined 
+              ?<img
+                className="phone__profile"
+                src={user.foto}
+             alt='Foto' />
+            :(user.foto = "https://res.cloudinary.com/df90q7vvj/image/upload/v1648860811/GestWork/23408e565fc3f43454636fec27572d1f_v8bhk3.jpg")}
+            </div>
+          </Form>
+   
+      </Container>
+    </Navbar>
+ 
 
     </div>
     <div id="servicios" className="block servicios">
