e50bdc0532f24bc79f4f6c10941f333ecbedb820
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-04-05 14:34:07 -0500

    styles logo nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,50 +2,51 @@ import React from "react";
 import { Container, Form, Navbar } from "react-bootstrap";
 import UserPerfil from "../hooks/UserPerfil";
 import "../styles/NavBar.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Notifications from "./Notifications";
 
-
 export const NavBar = () => {
-
   const user = UserPerfil();
-  console.log(user)
+  console.log(user);
 
   return (
-    <Navbar bg="light" className='container-fluid d-flex justify-content-end t-0'>
-      <Container fluid className='container-fluid d-flex justify-content-end t-0'>
-     {/* <Link to='/*'>  <img className="logoImg" style={{marginRight:"310px"}} src="https://res.cloudinary.com/df90q7vvj/image/upload/v1648513403/GestWork/Free_Sample_By_Wix_1_lvgyss.jpg" alt=""/>
-     </Link> */}
-               <Link to="/*">
-            {" "}
-            <img
-              className="logoImg mt-2"
-              style={{ width: "240px", marginRight: "320px" }}
-              src="https://res.cloudinary.com/dss4kjwzk/image/upload/v1649172010/GestWork/Gestwork_oieal5.png"
-              alt=""
-            />
-          </Link>
-          <Form className="sub_navBar">
-            <Link to="/profile" className="link-name">
+    <Navbar
+      bg="light"
+      className="container-fluid d-flex justify-content-end t-0"
+    >
+      <Container
+        fluid
+        className="container-fluid d-flex justify-content-end t-0"
+      >
+        <Link to="/*">
+          <img
+            className="logoImg mt-2"
+            style={{ width: "200px", marginRight: "365px" }}
+            src="https://res.cloudinary.com/dss4kjwzk/image/upload/v1649172010/GestWork/Gestwork_oieal5.png"
+            alt=""
+          />
+        </Link>
+        <Form className="sub_navBar">
+          <Link to="/profile" className="link-name">
             <div className="subtitle_navBar container-fluid d-flex justify-content-end">
-              <h4> {user.nombre !== undefined
-                  ? user.nombre
-                  : (user.nombre = "")}</h4>
+              <h4>
+                {" "}
+                {user.nombre !== undefined ? user.nombre : (user.nombre = "")}
+              </h4>
             </div>
-            </Link>
+          </Link>
 
-            <div className="d-flex p-1">
-              {user.foto !== undefined 
-              ?<img
-                className="phone__profile"
-                src={user.foto}
-             alt='Foto' />
-            :(user.foto = "https://res.cloudinary.com/dss4kjwzk/image/upload/v1649172010/GestWork/Gestwork_oieal5.png")}
-            </div>
-            {/* <Notifications/> */}
-          </Form>
-   
+          <div className="d-flex p-1">
+            {user.foto !== undefined ? (
+              <img className="phone__profile" src={user.foto} alt="Foto" />
+            ) : (
+              (user.foto =
+                "https://res.cloudinary.com/dss4kjwzk/image/upload/v1649172010/GestWork/Gestwork_oieal5.png")
+            )}
+          </div>
+          {/* <Notifications/> */}
+        </Form>
       </Container>
     </Navbar>
-  ); 
+  );
 };
